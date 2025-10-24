a59466841e02afa759e36b1d503df70a7ef16f95
Rajamani <m.a.rajamani@accenture.com>
2025-10-24 15:47:26 +0530

    Initial Commit

---
+++ src/test/java/com/magudeswaran/StudentsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magudeswaran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
