fffbec78e5aba769103e86d537e84366001f870b
Magudeswaran <rajamanimagudeswaran@gmail.com>
2025-11-08 14:38:38 +0530

    added end point to store professor to db

---
+++ src/main/java/com/magudeswaran/repository/ProfessorRepository.java
@@ -0,0 +1,9 @@
+package com.magudeswaran.repository;
+
+import com.magudeswaran.entity.Professor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfessorRepository extends JpaRepository<Professor, Integer> {
+
+
+}
---
+++ src/main/java/com/magudeswaran/service/AdminService.java
@@ -0,0 +1,26 @@
+package com.magudeswaran.service;
+
+import com.magudeswaran.dto.ProfessorDTO;
+import com.magudeswaran.entity.Professor;
+import com.magudeswaran.mapper.ProfessorModelMapper;
+import com.magudeswaran.repository.ProfessorRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AdminService {
+
+    private final ProfessorModelMapper professorModelMapper;
+    private final ProfessorRepository professorRepository;
+
+    public void addProfessor(ProfessorDTO professorDTO){
+
+        Professor prof = professorModelMapper.convertToEntity(professorDTO);
+
+        professorRepository.save(prof);
+
+
+    }
+
+}
